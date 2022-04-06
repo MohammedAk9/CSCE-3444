c9cc3cf48b9ba0ed8165bc9ca818bcd987830780
unknown <modydxx@gmail.com>
2022-04-06 14:36:44 -0500

    safe commit

---
+++ src/newtable.jsx
@@ -0,0 +1,246 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import { LicenseInfo } from '@mui/x-data-grid-pro';
+import {
+  useGridApiRef,
+  DataGridPro,
+  GridToolbarContainer,
+  GridActionsCellItem,
+} from '@mui/x-data-grid-pro';
+import {
+  randomCreatedDate,
+  randomTraderName,
+  randomUpdatedDate,
+  randomId,
+  
+} from '@mui/x-data-grid-generator';
+//<DataGrid experimentalFeatures={{ newEditingApi: true }} />;
+const rows = [
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    numberofpicks: 25,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+    password:123456,
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 36,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+    password:12345,
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 19,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+    password:12345,
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 28,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+    password:12345,
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 23,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+    password:12345,
+  },
+];
+
+function EditToolbar(props) {
+  const { apiRef } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    apiRef.current.updateRows([{ id, isNew: true }]);
+    apiRef.current.startRowEditMode({ id });
+
+    // Wait for the grid to render with the new row
+    setTimeout(() => {
+      apiRef.current.scrollToIndexes({
+        rowIndex: apiRef.current.getRowsCount() - 1,
+      });
+
+      apiRef.current.setCellFocus(id, 'name');
+    });
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add record
+      </Button>
+    </GridToolbarContainer>
+  );
+}
+
+EditToolbar.propTypes = {
+  apiRef: PropTypes.shape({
+    current: PropTypes.object.isRequired,
+  }).isRequired,
+};
+
+export default function FullFeaturedCrudGrid() {
+  const apiRef = useGridApiRef();
+
+  const handleRowEditStart = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleRowEditStop = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleEditClick = (id) => (event) => {
+    event.stopPropagation();
+    apiRef.current.startRowEditMode({ id });
+  };
+
+  const handleSaveClick = (id) => async (event) => {
+    event.stopPropagation();
+    await apiRef.current.stopRowEditMode({ id });
+  };
+
+  const handleDeleteClick = (id) => (event) => {
+    event.stopPropagation();
+    apiRef.current.updateRows([{ id, _action: 'delete' }]);
+  };
+
+  const handleCancelClick = (id) => async (event) => {
+    event.stopPropagation();
+    await apiRef.current.stopRowEditMode({ id, ignoreModifications: true });
+
+    const row = apiRef.current.getRow(id);
+    if (row.isNew) {
+      apiRef.current.updateRows([{ id, _action: 'delete' }]);
+    }
+  };
+
+  const processRowUpdate = async (newRow) => {
+    return { ...newRow, isNew: false };
+  };
+
+  const columns = [
+    { field: 'name', headerName: 'Name', width: 180, editable: true },
+    { field: 'age', headerName: 'Age', type: 'number', editable: true },
+    {
+      field: 'dateCreated',
+      headerName: 'Date Created',
+      type: 'date',
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'lastLogin',
+      headerName: 'Last Login',
+      type: 'dateTime',
+      width: 220,
+      editable: true,
+    },
+    {
+        field:'UserPassword',
+        headerName:'User Password',
+        type:'number',
+        width:180,
+        editable:true,
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = apiRef.current.getRowMode(id) === 'edit';
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              onClick={handleSaveClick(id)}
+              color="primary"
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  return (
+    <Box
+      sx={{
+        backgroundColor:"white",
+        height: 400,
+        width: '80%',
+        '& .actions': {
+         
+         
+        },
+        '& .textPrimary': {
+         
+          
+        },
+      }}
+    >
+      <DataGridPro
+        rows={rows}
+        columns={columns}
+        apiRef={apiRef}
+        editMode="row"
+        onRowEditStart={handleRowEditStart}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        background="white"
+        components={{
+          Toolbar: EditToolbar,
+        }}
+        componentsProps={{
+          toolbar: { apiRef },
+        }}
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box>
+  );
+}
