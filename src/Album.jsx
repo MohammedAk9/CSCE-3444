a5014144ba4a41226ebab1fd04f1a3976a5a773e
MohammedAk9 <77897095+MohammedAk9@users.noreply.github.com>
2022-04-08 18:25:59 -0500

    Update Album.jsx

--- src/Album.jsx
+++ src/Album.jsx
@@ -73,22 +73,7 @@ function Album() {
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        
-      </Box>
-      {/* End footer */}
+     
     </ThemeProvider>
   );
 }
